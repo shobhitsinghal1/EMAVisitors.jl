3f3bb5acdb7dece17f4fc5affe6cd7e956487e1e
licioromao@gmail.com <licio.romao@gmail.com>
2025-06-18 10:48:45 +0200

    Working on documentation

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,8 @@ using Documenter, EMAVisitors
 makedocs(
     sitename = "EMAVisitors.jl",
     authors = "EMA team",
+    edit_branch = "main",  # Set to your default branch ("main" or "master")
+    repo = Documenter.Remotes.GitHub("licioromao", "EMAVisitors.jl"),
     modules = [EMAVisitors],
     pages = [
         "Home" => "index.md",
